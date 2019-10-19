676bbefab6e9f684c1460becec46c5deb2cd59dc
田中翔偉 <tanakashoi@tanakashouinoMacBook-Pro.local>
2019-10-19 16:14:00 +0900

    first commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by 田中翔偉 on 2019/10/19.
+//  Copyright © 2019 ManchestarCity. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
